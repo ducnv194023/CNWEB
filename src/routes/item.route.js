49c312b2f5db2e6cfd0136a1d8153a8992037c54
duc <duc.nv194023@sis.hust.edu.vn>
2022-07-21 14:15:16 +0700

    feat: fix route item

--- src/routes/item.route.js
+++ src/routes/item.route.js
@@ -15,7 +15,7 @@ router.route('/pagination').post(auth, validate(itemValidation.getItems), itemCo
 // người dùng mua vé bơi, thêm thông tin người dùng vào trong vé
 router.route('/sign-ticket').post(auth, validate(itemValidation.signTicket), itemController.signTicket)
 // lấy ra vé đã mua của người dùng
-router.route('owner-ticket').post(auth, validate(itemValidation.getOwnerTicket), itemController.getOwnerTicket)
+router.route('/owner-ticket').post(auth, validate(itemValidation.getOwnerTicket), itemController.getOwnerTicket)
 
 router.route('/:itemId')
 // Lấy ra một danh mục
